737615cea8c604e65e7a0af1d79439b1c8169ac3
MuhammedM294 <muhammedaabdelaal@gmail.com>
2024-05-13 12:45:54 +0200

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 if __name__ == "__main__":
     try:
         setup(
-            version="0.5.0",
+            version="1.0.1",
             entry_points={
                 "console_scripts": [
                     "run = smadi.workflow:main",
